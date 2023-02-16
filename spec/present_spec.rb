978219553e2cbaa163a75835edbc2cab70456c6e
lizaebadi <liza-1@hotmail.co.uk>
2023-02-16 10:21:24 +0000

    Correct version Testing Errors 2nd Exercise

--- spec/present_spec.rb
+++ spec/present_spec.rb
@@ -3,11 +3,14 @@
 require 'present'
 
 RSpec.describe Present do 
-  context "When a present has already been wrapped" do 
-    it "fails" do 
+    it "fails if we unwrap without wrapping" do 
+      present = Present.new
+      expect { present.unwrap }.to raise_error "No contents have been wrapped."
+    end 
+
+    it "fails if we wrap when we have already wrapped" do 
       present = Present.new
       present.wrap("bike")
-      expect { present.wrap("bike") }.to raise_error "A contents has already been wrapped."
+      expect { present.wrap("football") }.to raise_error "A contents has already been wrapped."
     end 
-  end 
 end 
